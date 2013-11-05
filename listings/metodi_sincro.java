b5415d69a1f2fc8d48ecceb5f97f8ab3751b87f4
Marco <marco.pezzutti@hotmail.it>
2013-11-06 00:22:35 +0100

    stesura sviluppo, pianificazione  completa, inizio stesura bibliografia

---
+++ listings/metodi_sincro.java
@@ -0,0 +1,25 @@
+/**
+* Metodo che scarica i dati presenti nel server
+* per ogni store presente nel registro.
+*/
+downloadFromServer: function()
+{
+      for (i = 0; i < this.getRegistry().length; i++)
+      {
+	      var store = this.getRegistry()[i];
+	      this.getStore(store).download();
+      }
+},
+
+/**
+* Metodo che carica i dati aggiornati sul server
+* per ogni store presente nel registro.
+*/
+uploadToServer: function()
+{
+      for (i = 0; i < this.getRegistry().length; i++)
+      {
+	      var store = this.getRegistry()[i];
+	      this.getStore(store).upload();
+      }
+}
